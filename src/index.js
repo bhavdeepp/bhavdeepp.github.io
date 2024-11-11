b8792f1fa5bdb4b8b461349fb75da76202f0dcdc
singhbhavdeep2434 <singh.bhavdeep2434@gmail.com>
2024-11-10 23:24:08 -0500

    changes in index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
-import { render } from 'react-dom'
-import App from './App'
-import { ThemeProvider } from './contexts/theme'
-import './index.css'
+// import { render } from 'react-dom'
+// import App from './App'
+// import { ThemeProvider } from './contexts/theme'
+// import './index.css'
 
-render(
+// render(
+//   <ThemeProvider>
+//     <App />
+//   </ThemeProvider>,
+//   document.getElementById('root')
+// )
+
+
+import ReactDOM from 'react-dom/client';  // Updated import
+import App from './App';
+import { ThemeProvider } from './contexts/theme';
+import './index.css';
+
+// Create a root element and render your app
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <ThemeProvider>
     <App />
-  </ThemeProvider>,
-  document.getElementById('root')
-)
+  </ThemeProvider>
+);
