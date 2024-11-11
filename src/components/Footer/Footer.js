dad8a7b97cd6cbb8f1929c3b25c82c60baf08275
singhbhavdeep2434 <singh.bhavdeep2434@gmail.com>
2024-11-10 22:49:43 -0500

    Portfolio upadte

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,10 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/rjshkhr/cleanfolio'
+      href='https://github.com/'
       className='link footer__link'
     >
-      Created By Raj Shekhar
+      Bhavdeep Singh
     </a>
   </footer>
 )
--- src/portfolio.js
+++ src/portfolio.js
@@ -1,19 +1,19 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rjshkhr.github.io/cleanfolio',
+  homepage: 'https://bhavdeepp.github.io/',
   title: 'JS.',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Bhavdeep Singh',
+  role: 'Full Stack Developer',
   description:
     'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
   resume: 'https://example.com',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/codebhavdeep/',
+    github: 'https://github.com/bhavdeepp',
   },
 }
 
@@ -65,7 +65,7 @@ const skills = [
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'singh.bhavdeep2434@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
